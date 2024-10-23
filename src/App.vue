01a850d0302c7382c6da6b9b598813c13a4ca0c9
estrella <estrelladelpilarticonapanduro@gmail.com>
2024-10-23 13:06:14 -0500

    feat add: register and login

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
-<script setup>
+<script>
+import registercomponet from './iam/components/register.component.vue'
+/* import loginComponent from './iam/components/login.component.vue'; */
+
+export default {
+    components:{
+        //loginComponent
+        registercomponet
+    }
+}
+
 </script>
 
 <template>
+    <registercomponet></registercomponet>
+   <!--  <loginComponent></loginComponent> -->
 </template>
 
 <style scoped>
