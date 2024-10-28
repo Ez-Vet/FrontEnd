57899f3f41777236f478ed4e38e572ff33a88a8d
estrella <estrelladelpilarticonapanduro@gmail.com>
2024-10-28 13:50:22 -0500

    feat: Add sidebar layout component

---
+++ src/public/pages/sidebarlayout.component.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="sidebar-layout">
+        <sidebarComponent />
+        <router-view />
+    </div>
+</template>
+
+<script>
+import sidebarComponent from '../sidebar.component.vue';
+
+export default {
+    name: 'SidebarLayout',
+    components: { sidebarComponent }
+}
+</script>
+
+<style>
+/* .sidebar-layout {
+    display: flex;
+}
+
+.sidebar {
+    width: 250px;
+    background-color: #f4f4f4;
+    padding: 20px;
+}
+
+.main-content {
+    flex-grow: 1;
+    padding: 20px;
+} */
+body {
+    background: #8BDCD6;
+}
+</style>
+
+<style scoped>
+.sidebar-layout {
+    display: flex;
+}
+</style>
